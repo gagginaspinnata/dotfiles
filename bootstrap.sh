c2da044768798a1a7c7077ac866197915aa3ff78
Mauro Marano <mauromarano@MacBook-Pro-di-Mauro.local>
2013-12-02 11:59:34 +0100

    The bootstrap files now let you guide through the installations process

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
+clear
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_profile vimrc osx gitignore"    # list of files/folders to symlink in homedir
+echo "           __      __  _____ __
+      ____/ /___  / /_/ __(_) /__  _____
+     / __  / __ \/ __/ /_/ / / _ \/ ___/
+    / /_/ / /_/ / /_/ __/ / /  __(__  )
+    \__,_/\____/\__/_/ /_/_/\___/____/
+                                        "
 
-##########
+echo "\n\n Choose if you want to install [z]sh or [b]ash dotfiles: "
+read shell
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+if [[ "$shell" == "b" ]]; then
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
-
-
+    bash install.sh
+else
+    git checkout zsh
+    bash install.sh
