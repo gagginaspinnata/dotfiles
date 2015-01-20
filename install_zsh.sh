743791a72ef7122d844e8bca56dd333828806c25
gagginaspinnata <gagginaspinnata@gmail.com>
2015-01-20 16:21:56 +0100

    updates

--- install_zsh.sh
+++ install_zsh.sh
@@ -36,10 +36,12 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 ln -s $dir/vimrc ~/.vimrc
 
 # changing the default theme
-cat ~/.zshrc | sed 's~\(ZSH_THEME="\)[^"]*\(".*\)~\1dpoggi\2~' > ~/.zshrc
+#cat ~/.zshrc | sed 's~\(ZSH_THEME="\)[^"]*\(".*\)~\1dpoggi\2~' > ~/.zshrc
 
 #setting 256 term color
 echo "export TERM=xterm-256color">>~/.zshrc
 echo '[ -n "$TMUX" ] && export TERM=screen-256color'>>~/.zshrc
 
 echo "Remember to run sudo :PluginInstall inside vim to install nerdtree"
+
+echo "Remember to change the zsh theme in the file .zshrc a nice one is the dpoggi"
